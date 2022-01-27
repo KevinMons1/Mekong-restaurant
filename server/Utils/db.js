ed3fecf1c55555702ae84b3174abfb45dc36a6a4
KevinMons1 <kevin2001@hotmail.be>
2022-01-27 13:32:45 +0100

    try to correct lost of connection

--- server/Utils/db.js
+++ server/Utils/db.js
@@ -1,14 +1,7 @@
 require("dotenv")
 const mysql = require("mysql2")
 
- let db = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.DB_HOST_JW,
-    user: process.env.DB_USER_JW,
-    password: process.env.DB_PASSWORD_JW,
-    database: process.env.DB_DATABASE_JW,
-    port: process.env.DB_PORT_JW
-})
+let db = mysql.createPool(process.env.DB_URL_JW)
 
 // Connect
 db.getConnection((err) => {
