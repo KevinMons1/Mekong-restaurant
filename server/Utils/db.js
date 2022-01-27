3847f94f5a5a660aef2cb1ceaab33bd5564038e5
KevinMons1 <kevin2001@hotmail.be>
2022-01-27 09:21:19 +0100

    try to correct lost of connection

--- server/Utils/db.js
+++ server/Utils/db.js
@@ -1,12 +1,49 @@
+// require("dotenv")
+// const mysql = require("mysql2")
+
+//  let db = mysql.createPool({
+//     connectionLimit: 10,
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+// })
+    
+// // Connect
+// db.getConnection((err) => {
+//     if (err) {
+//         throw err
+//     }
+//     console.log("Mysql connected...")
+// })
+
+// module.exports = db
+
 require("dotenv")
 const mysql = require("mysql2")
 
- let db = mysql.createPool({
-    connectionLimit: 10,
+const mysqlConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
-})
+}
+
+db = mysql.createConnection(mysqlConfig)
+
+const handleDisconnect = (client) => {
+    client.on('error', (err) => {
+        if (!err.fatal) return
+        if (err.code !== "PROTOCOL_CONNECTION_LOST") throw err
+
+        console.error('> Re-connecting lost MySQL connection: ' + error.stack)
+
+        db = mysql.createConnection(mysqlConfig)
+        handleDisconnect(mysqlClient)
+        mysqlClient.connect()
+    })
+}
+
+handleDisconnect(db)
 
 module.exports = db
